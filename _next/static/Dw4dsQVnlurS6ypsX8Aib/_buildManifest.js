9f5f53459989f7499e1ecc757a9d08d601a060c6
christiantochen <christiantochen@users.noreply.github.com>
2021-11-28 10:22:08 +0000

    deploy: 7886261fce5447ca8bdf880eebd42869fb4a23e0

--- _next/static/Dw4dsQVnlurS6ypsX8Aib/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/78e521c3-320d3b053b9e1e9a.js","static/css/79286ff581a14d98.css","static/chunks/pages/index-23c992fada08b8b1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
